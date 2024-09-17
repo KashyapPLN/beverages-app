2925cd6d1fd0e0cab09844d3c2c644773670587c
KashyapPLN <kashyap.pln@gmail.com>
2024-09-17 20:24:54 +0530

    Added redux toolkit and store.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,17 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './navbar.css';
 import logo from '../assets/logo.png'
-import { Button, Form, Modal } from 'react-bootstrap';
+import { Button, Dropdown, Form, Modal } from 'react-bootstrap';
 import { IoCartOutline } from 'react-icons/io5';
 import { CiSearch } from 'react-icons/ci';
 import Login from './login/Login';
 import Signup from './login/Signup';
-
+import { useAuth0 } from '@auth0/auth0-react';
+import { useDispatch } from 'react-redux';
+import { setCredentials,clearCredentials } from '../features/auth/authSlice';
+import { useSelector } from 'react-redux';
 
 export default function NavBar() {
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
+  
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
+  const dispatch = useDispatch();
+  const userProfile = useSelector((state) => state.auth.user);
   const handleShow = () => {setShow(true);setLogin(true)};
+
+  useEffect(()=>{
+    if(isAuthenticated){
+      dispatch(setCredentials({ user: user }));
+      console.log('user is',user);
+    }
+  },[isAuthenticated])
+
+  const handleLogin = () => {
+    loginWithRedirect();
+      };
+  const handleLogout = () => {
+    logout({ returnTo: window.location.origin });
+    dispatch(clearCredentials());
+  };
   const [login,setLogin]=useState(true);
   return (
     <div className='navbar'>
@@ -22,7 +44,18 @@ export default function NavBar() {
 <div className='navbar-buttons'>
 <Button className='navbar-icon-button'variant='text'><CiSearch /></Button>
 <Button className='navbar-icon-button'variant='text'><IoCartOutline /></Button>
-<button className='login' onClick={handleShow}>login</button>
+{!isAuthenticated ? <button className='login' onClick={handleLogin}>login</button> : 
+<Dropdown>
+      <Dropdown.Toggle variant="text">
+     {userProfile&&userProfile.nickname}
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={handleLogout}>logout</Dropdown.Item>
+        
+      </Dropdown.Menu>
+    </Dropdown>
+}
 </div>
 <Modal className='login-modal'show={show} onHide={handleClose} centered>
   <Modal.Header className='login-modal-header' closeButton>
@@ -32,6 +65,6 @@ export default function NavBar() {
   :<Signup setLogin={setLogin} handleClose={handleClose}/>
   }
 </Modal>
-    </div>
+         </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { Provider } from 'react-redux';
+import store from './Store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,7 +17,9 @@ root.render(
         redirect_uri: window.location.origin
       }}
     >
+      <Provider store={store}>
       <App />
+      </Provider>
     </Auth0Provider>
   </BrowserRouter>
 );
